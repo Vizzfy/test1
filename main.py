d82630c33e4c04b9fd7674f9d7be12174e6691a6
IM23a-anastasiak <anastasiak@bzz,ch>
2024-03-26 19:46:49 +0100

    insert_data in ein eigenes File gepackt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from insert_datat1 import insert_data
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -8,18 +9,7 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 mycursor.execute('INSERT INTO Beispiel (schluessel, Vorname, Nachname) VALUES (1, "Kristian", "Anastasia")')
-def insert_data(Vorname, Nachname):
-    sql = "INSERT INTO Beispiel2 (schluessel2, Vorname2, Nachname2) VALUES (2, %s, %s)"
-    val = (Vorname, Nachname)
-    mycursor.execute(sql, val)
-    mydb.commit()
-    print(mycursor.rowcount, "Datensatz eingefügt.")
 
-# Benutzereingabe für Namen und Alter abfragen
-Vorname = input("Bitte gib deinen Vornamen ein: ")
-Nachname = input("Bitte gib deinen Nachnamen ein: ")
-
-insert_data(Vorname, Nachname)
 
 mydb.commit()
 mycursor.close()
